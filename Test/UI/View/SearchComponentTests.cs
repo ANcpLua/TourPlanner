495d8faaad2d75a5dfbfea8adfdb15fc81d5df09
anfh2 <anfh22@outlook.com>
2025-06-15 11:44:39 +0200

    # This is a combination of 4 commits. # This is the 1st commit message:
    view tests left
    
    # This is the commit message #2:
    
    maybe include view?
    
    # This is the commit message #3:
    
    .NET 9
    
    # This is the commit message #4:
    
    Merge remote-tracking branch 'origin/main'
    
    Merge remote-tracking branch 'origin/main'

--- Test/UI/View/SearchComponentTests.cs
+++ Test/UI/View/SearchComponentTests.cs
@@ -10,7 +10,7 @@ namespace Test.UI.View;
 public sealed class SearchComponentTests : BunitTestBase
 {
     [Test]
-    public void BindsSearchText()
+    public void SearchInput_TypesText_UpdatesViewModel()
     {
         var cut = RenderComponent<SearchComponent>();
         cut.Find("input.search-input").Input("test");
@@ -19,13 +19,12 @@ public sealed class SearchComponentTests : BunitTestBase
     }
 
     [Test]
-    public async Task EnterKeyTriggersSearch()
+    public async Task SearchInput_PressesEnter_TriggersSearch()
     {
         Services.Mock<IHttpService>().Setup(h => h.GetListAsync<Tour>("api/tour/search/test"))
             .ReturnsAsync(TestData.SampleTourList(0));
-
-        var cut = RenderComponent<SearchComponent>();
         Services.ViewModel<SearchViewModel>().SearchText = "test";
+        var cut = RenderComponent<SearchComponent>();
 
         await cut.Find("input.search-input").KeyUpAsync(new KeyboardEventArgs { Key = "Enter" });
 
@@ -33,12 +32,12 @@ public sealed class SearchComponentTests : BunitTestBase
     }
 
     [Test]
-    public async Task SearchResultsNavigateOnClick()
+    public async Task TourCard_Clicks_NavigatesToTour()
     {
         var tour = TestData.SampleTour();
         Services.ViewModel<SearchViewModel>().SearchResults = [tour];
-
         var cut = RenderComponent<SearchComponent>();
+
         await cut.Find("div.tour-search").ClickAsync(new MouseEventArgs());
 
         var nav = Services.GetRequiredService<NavigationManager>();
@@ -46,11 +45,10 @@ public sealed class SearchComponentTests : BunitTestBase
     }
 
     [Test]
-    public async Task EmptySearchShowsNotification()
+    public async Task SearchButton_NoResults_ShowsNotification()
     {
         Services.Mock<IHttpService>().Setup(h => h.GetListAsync<Tour>("api/tour/search/test"))
             .ReturnsAsync(new List<Tour>());
-
         Services.ViewModel<SearchViewModel>().SearchText = "test";
         var cut = RenderComponent<SearchComponent>();
 
@@ -58,5 +56,134 @@ public sealed class SearchComponentTests : BunitTestBase
 
         Services.Mock<IToastServiceWrapper>()
             .Verify(t => t.ShowSuccess("No tours found matching your search criteria."));
+    }
+
+    [TestCase(1d, 4d, "Yes")]
+    [TestCase(2d, 3d, "Yes")]
+    [TestCase(2d, 4d, "Yes")]
+    [TestCase(3d, 4d, "No")]
+    [TestCase(1d, 2d, "No")]
+    public void TourWithLog_HasDifficultyAndRating_DisplaysChildFriendlyStatus(double difficulty, double rating,
+        string expectedStatus)
+    {
+        var tour = TestData.SampleTour();
+        tour.TourLogs.Clear();
+        tour.TourLogs.Add(new TourLog
+        {
+            TourId = tour.Id, Difficulty = difficulty, Rating = rating, DateTime = DateTime.Now, Comment = "Test log",
+            TotalDistance = 10, TotalTime = 60
+        });
+        Services.ViewModel<SearchViewModel>().SearchResults = [tour];
+
+        var cut = RenderComponent<SearchComponent>();
+
+        Assert.That(cut.Find("div.tour-search").TextContent, Does.Contain($"Child Friendly: {expectedStatus}"));
+    }
+
+    [Test]
+    public void TourWithoutLogs_HasEmptyCollection_DisplaysNotChildFriendly()
+    {
+        var tour = TestData.SampleTour();
+        tour.TourLogs.Clear();
+        Services.ViewModel<SearchViewModel>().SearchResults = [tour];
+
+        var cut = RenderComponent<SearchComponent>();
+
+        Assert.That(cut.Find("div.tour-search").TextContent, Does.Contain("Child Friendly: No"));
+    }
+
+    [TestCase("Beautiful scenic route", "Beautiful scenic route")]
+    [TestCase("", "N/A")]
+    public void TourDescription_HasValue_DisplaysCorrectly(string description, string expectedText)
+    {
+        var tour = TestData.SampleTour();
+        tour.Description = description;
+        Services.ViewModel<SearchViewModel>().SearchResults = [tour];
+
+        var cut = RenderComponent<SearchComponent>();
+
+        Assert.That(cut.Find("div.tour-search").TextContent, Does.Contain($"Description: {expectedText}"));
+    }
+
+    [Test]
+    public void TourDescription_IsNull_DisplaysNA()
+    {
+        var tour = TestData.SampleTour();
+        tour.Description = null;
+        Services.ViewModel<SearchViewModel>().SearchResults = [tour];
+
+        var cut = RenderComponent<SearchComponent>();
+
+        Assert.That(cut.Find("div.tour-search").TextContent, Does.Contain("Description: N/A"));
+    }
+
+    [TestCase("/images/tour.jpg", "Available")]
+    [TestCase("", "N/A")]
+    public void TourImagePath_HasValue_DisplaysCorrectly(string imagePath, string expectedText)
+    {
+        var tour = TestData.SampleTour();
+        tour.ImagePath = imagePath;
+        Services.ViewModel<SearchViewModel>().SearchResults = [tour];
+
+        var cut = RenderComponent<SearchComponent>();
+
+        Assert.That(cut.Find("div.tour-search").TextContent, Does.Contain($"Image: {expectedText}"));
+    }
+
+    [Test]
+    public void TourImagePath_IsNull_DisplaysNA()
+    {
+        var tour = TestData.SampleTour();
+        tour.ImagePath = null;
+        Services.ViewModel<SearchViewModel>().SearchResults = [tour];
+
+        var cut = RenderComponent<SearchComponent>();
+
+        Assert.That(cut.Find("div.tour-search").TextContent, Does.Contain("Image: N/A"));
+    }
+
+    [Test]
+    public void TourWithMultipleLogs_HasLatestFirst_DisplaysLatestLogInfo()
+    {
+        var tour = TestData.SampleTour();
+        tour.TourLogs.Clear();
+
+        var olderLog = TestData.SampleTourLog(3, 1, tour.Id);
+        olderLog.DateTime = DateTime.Now.AddDays(-5);
+
+        var newerLog = TestData.SampleTourLog(5, 1, tour.Id);
+        newerLog.DateTime = DateTime.Now.AddDays(-1);
+
+        tour.TourLogs.Add(olderLog);
+        tour.TourLogs.Add(newerLog);
+
+        Services.ViewModel<SearchViewModel>().SearchResults = [tour];
+
+        var cut = RenderComponent<SearchComponent>();
+
+        var tourCard = cut.Find("div.tour-search");
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(tourCard.TextContent, Does.Contain("Latest Log:"));
+            Assert.That(tourCard.TextContent, Does.Contain(newerLog.DateTime.ToShortDateString()));
+            Assert.That(tourCard.TextContent, Does.Contain("Log Rating: 5"));
+        }
+    }
+
+    [Test]
+    public void TourWithoutLogs_HasEmptyCollection_HidesLogInfo()
+    {
+        var tour = TestData.SampleTour();
+        tour.TourLogs.Clear();
+        Services.ViewModel<SearchViewModel>().SearchResults = [tour];
+
+        var cut = RenderComponent<SearchComponent>();
+
+        var tourCard = cut.Find("div.tour-search");
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(tourCard.TextContent, Does.Not.Contain("Latest Log:"));
+            Assert.That(tourCard.TextContent, Does.Not.Contain("Log Rating:"));
+        }
     }
 }
